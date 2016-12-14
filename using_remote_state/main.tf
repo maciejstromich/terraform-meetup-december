a8c8abba3f60979173face9eddd539fe40d84780
maartenvanderhoef <maartenvanderhoef@Maartens-MacBook-Pro.local>
2016-12-14 22:44:25 +0100

    clean up

--- using_remote_state/main.tf
+++ using_remote_state/main.tf
@@ -55,6 +55,6 @@ resource "aws_instance" "web" {
     ami = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
     tags {
-        Name = "TF-MEETUP"
+        Name = "${var.shortname}"
     }
 }
--- using_remote_state/variables.tf
+++ using_remote_state/variables.tf
@@ -15,13 +15,3 @@ variable "shortname" {
   description = "The name for everything related to this setup"
   default     = "tfmeetup"
 }
-
-variable "vpc_cidr" {
-  description = "The CIDR of the network created in the VPC"
-  default     = "172.16.0.0/16"
-}
-
-variable "subnet_cidr" {
-  description = "The CIDR of the subnet created in the VPC"
-  default     = "172.16.1.0/24"
-}
