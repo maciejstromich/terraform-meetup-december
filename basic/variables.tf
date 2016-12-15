b743423760749e98be0b5bf8aeee10d533dae93a
maartenvanderhoef <maartenvanderhoef@Maartens-MacBook-Pro.local>
2016-12-15 11:45:10 +0100

    Improvements

--- basic/variables.tf
+++ basic/variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  description = "The AWS Access key"
-}
-
-variable "secret_key" {
-  description = "The AWS secret key"
-}
-
 variable "region" {
   description = "The AWS region"
   default = "us-west-2"
